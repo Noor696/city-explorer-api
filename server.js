d92e0a28ed1d184c78719995f2fca4660586dc59
Noor696 <eng.nooralhateeb@gmail.com>
2022-08-30 09:33:33 +0300

    not finish lab07

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express'); //import express framework
 const cors = require('cors'); //import cors library
 const server = express(); 
 require('dotenv').config(); //import dotenv library
-
+const weatherData = require('./data/weather.json');
 // local IP address
 // port
 
-// Global route
+// Global route (always upper one)
 //http:localhost:PORT/
-//ex: www.google.com (home page)
+//ex: www.google.com (home route)
 server.get('/',(req,res)=>{
     res.send("Hi from the home route");
 })
@@ -19,6 +19,11 @@ const PORT = 3000
 server.get('/route',(req,res)=>{
     res.send("Hi from the my route");
 })
+
+//http:localhost:PORT/getweather
+server.get('/getweather',(req,res)=>{
+    
+});
 
 server.listen(PORT, () => {
     console.log(`Hello, I am listening on ${PORT}`);
